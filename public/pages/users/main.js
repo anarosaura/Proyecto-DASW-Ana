581c6a1e2acd30fd026721c6f4a5eb833c2f4dc0
Ana Rosaura <rosaurazam896@gmail.com>
2023-12-02 02:12:30 -0600

    Update de carpeta public

--- public/pages/users/main.js
+++ public/pages/users/main.js
@@ -1,60 +1,50 @@
 console.log("hola");
 
-(function(){
+(function () {
 
-    const $form = $('form').eq(0);
-    
-    const $photoContainer = $('#photoContainer');
     const $fileInput = $('#fileInput');
-    const $button = $('#submit');
-
-    // Evento de clic en la foto por defecto
-    $photoContainer.on('click', function () {
-        $fileInput.click();
-    });
+    const $imagenPerfil = $('#imagenPerfil');
 
     // Evento de cambio en el input de tipo archivo
     $fileInput.on('change', function () {
         const archivoSeleccionado = $fileInput[0].files[0];
-        $button.attr('disabled', !archivoSeleccionado);
-    });
 
-    $form.on('submit', (e) => {
-        e.preventDefault();
-
-        const archivoSeleccionado = $fileInput[0].files[0];
-            
-        if(archivoSeleccionado){
-            const form = new FormData();
-            form.append('archivo', archivoSeleccionado);
-
-            $.ajax({
-                url: 'http://localhost:3000/upload',
-                type: 'POST',
-                mimeType: 'multipart/form-data',
-                processData: false,
-                contentType: false,
-                data: form,
-                success: (res) => {
-                    alert('Se subió el archivo.');
-                    console.log(res);
-                    const fileName = res;
-                    const imageUrl = `http://localhost:3000/uploads/${fileName}`;
-                    
-                    // Actualiza la ruta de la imagen
-                    $('#imagenPerfil').attr('src', imageUrl);
-                },
-                error: (err) => {
-                    console.log('Error: ', err);
-                    alert('Algo salió mal.');
-                }
-            }).then((res) => {
-                
-            })
-
-        } else {
+        // Verifica si hay un archivo seleccionado antes de continuar con la lógica de subir la imagen
+        if (!archivoSeleccionado) {
             alert('No has seleccionado ningún archivo.');
+            return; // Salir del evento si no hay archivo seleccionado
         }
-        
+
+        // Continuar con la lógica de subir la imagen
+        const form = new FormData();
+        form.append('archivo', archivoSeleccionado);
+
+        $.ajax({
+            url: 'http://localhost:3000/upload',
+            type: 'POST',
+            mimeType: 'multipart/form-data',
+            processData: false,
+            contentType: false,
+            data: form,
+            success: (res) => {
+                alert('Se subió el archivo.');
+                console.log(res);
+                const fileName = res;
+                const imageUrl = `http://localhost:3000/uploads/${fileName}`;
+
+                // Actualiza la ruta de la imagen en el elemento img
+                $imagenPerfil.attr('src', imageUrl);
+            },
+            error: (err) => {
+                console.log('Error: ', err);
+                alert('Algo salió mal.');
+            }
+        });
     });
+
+    // Evento de clic en la foto por defecto
+    $imagenPerfil.on('click', function () {
+        $fileInput.click();
+    });
+
 })();
